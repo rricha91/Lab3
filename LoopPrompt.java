9565cc88b730ae75dd16232734ec55c149b37935
rricha91 <60485491+rricha91@users.noreply.github.com>
2020-02-18 18:12:25 -0500

    Update LoopPrompt.java

--- LoopPrompt.java
+++ LoopPrompt.java
@@ -4,16 +4,19 @@ public class LoopPrompt {
 		char awn;
 		Scanner inp = new Scanner(System.in);
 		do {
+			System.out.print("\n\tWould you like to run the program again? (y/n): ");
 			awn = Character.toUpperCase(inp.next().charAt(0));
 		} while (!valid(awn));
+		inp.nextLine();
 		inp.close();
 		return awn;
 	}
 
 	private boolean valid(char a) {
 		if (a==89||a==78) return true;
-		else System.out.print("\n\tInvalid Input, please enter something else."
-				+ "\n\tWould you like to run the program again? (y/n): ");
-		return false;
+		else {
+			System.out.print("\tInvalid Input, please enter something else.");
+			return false;
+		}
 	}
 }
